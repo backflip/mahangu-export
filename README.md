1245158105dfd366e17108395d2b801b51488972
Thomas Jaggi <thomas@responsive.ch>
2021-01-17 20:38:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Mahangu export
+
+Export trip from mahangu.com.
+
+## Setup
+
+Requires Node.js, tested on v14.
+
+`npm install`
+
+## Run
+
+`npm run export -- --trip ID`
---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const del = require("del");
+const minimist = require("minimist");
+
+const { saveMeta } = require("./lib/meta");
+const { saveWaypoints } = require("./lib/waypoints");
+const { saveScreenshots } = require("./lib/screenshots");
+
+const { trip } = minimist(process.argv.slice(2));
+
+const DIR = "build";
+
+(async function main() {
+  try {
+    await del(DIR);
+    fs.mkdirSync(DIR);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setViewport({
+      width: 1600 + 393, // Map width + content area
+      height: 900, // Map height
+    });
+
+    await page.goto(`https://www.mahangu.com/en/trip/${trip}`, {
+      waitUntil: "networkidle2",
+    });
+
+    await saveMeta({ page, directory: DIR });
+    await saveWaypoints({ page, directory: DIR });
+    await saveScreenshots({ page, directory: `${DIR}/screenshots` });
+
+    await browser.close();
+  } catch (err) {
+    console.error(err);
+  }
+})();

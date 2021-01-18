b1959cef18ba56e33a9db2711b717eefbe2b5d54
Thomas Jaggi <thomas@responsive.ch>
2021-01-18 23:51:54 +0100

    feat: add --skipDownloads flag

--- index.js
+++ index.js
@@ -9,67 +9,72 @@ const { saveScreenshots } = require("./lib/screenshots");
 const { savePhotos } = require("./lib/photos");
 const { saveHtml } = require("./lib/html");
 
-const { trip, debug, clean } = minimist(process.argv.slice(2));
+const { trip, debug, clean, skipDownloads } = minimist(process.argv.slice(2));
 
 const DIR = "build";
 
 (async function main() {
   try {
-    if (clean) {
-      await del(DIR);
-    }
+    let meta, waypoints, photos;
 
-    if (!fs.existsSync(DIR)) {
-      fs.mkdirSync(DIR);
-    }
+    if (!skipDownloads) {
+      if (clean) {
+        await del(DIR);
+      }
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+      if (!fs.existsSync(DIR)) {
+        fs.mkdirSync(DIR);
+      }
 
-    if (debug) {
-      await page.setRequestInterception(true);
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
 
-      page.on("request", (request) => {
-        console.log(">>", request.method(), request.url());
+      if (debug) {
+        await page.setRequestInterception(true);
 
-        request.continue();
+        page.on("request", (request) => {
+          console.log(">>", request.method(), request.url());
+
+          request.continue();
+        });
+      }
+
+      await page.setViewport({
+        width: 1600 + 393, // Map width + content area
+        height: 900, // Map height
       });
+
+      await page.goto(`https://www.mahangu.com/en/trip/${trip}`, {
+        waitUntil: "networkidle2",
+      });
+
+      // Meta data
+      meta = await saveMeta({ page, directory: DIR });
+
+      // Waypoints
+      waypoints = await saveWaypoints({ page, directory: DIR });
+
+      // Photos
+      photos = await savePhotos({
+        directory: `${DIR}/photos`,
+        waypoints,
+        trip,
+      });
+
+      // Screenshots of specific UI elements
+      await saveScreenshots({
+        page,
+        trip,
+        waypoints,
+        directory: `${DIR}/screenshots`,
+      });
+
+      await browser.close();
+    } else {
+      meta = require(`./${DIR}/meta.json`);
+      waypoints = require(`./${DIR}/waypoints.json`);
+      photos = require(`./${DIR}/photos.json`);
     }
-
-    await page.setViewport({
-      width: 1600 + 393, // Map width + content area
-      height: 900, // Map height
-    });
-
-    await page.goto(`https://www.mahangu.com/en/trip/${trip}`, {
-      waitUntil: "networkidle2",
-    });
-
-    // Meta data
-    const meta = await saveMeta({ page, directory: DIR });
-    // const meta = require("./build/meta.json");
-
-    // Waypoints
-    const waypoints = await saveWaypoints({ page, directory: DIR });
-    // const waypoints = require("./build/waypoints.json");
-
-    // Photos
-    const photos = await savePhotos({
-      directory: `${DIR}/photos`,
-      waypoints,
-      trip,
-    });
-    // const photos = require("./build/photos.json");
-
-    // Screenshots of specific UI elements
-    await saveScreenshots({
-      page,
-      trip,
-      waypoints,
-      directory: `${DIR}/screenshots`,
-    });
-
-    await browser.close();
 
     // Generate HTML
     await saveHtml({ meta, waypoints, photos, directory: DIR });
